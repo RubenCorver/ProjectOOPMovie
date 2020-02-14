498d576a1a722aad27a9db258e941e03a83dc9a1
Nino <nino@caiway.net>
2020-02-14 12:47:31 +0100

    Register

---
+++ Website/Register.php
@@ -0,0 +1,24 @@
+<?php
+
+define('DB_HOST', 'remotemysql.com');
+define('DB_NAME', 'aQ4VHAUFSM');
+define('DB_USER', 'aQ4VHAUFSM');
+define('DB_PASSWORD', 'L8fuvCAclt');
+
+$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " .     mysql_error());
+$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+
+
+
+$userName = $_POST['username'];
+$password =  $_POST['password'];
+$query = "INSERT INTO user (username,password) VALUES ('$userName','$password')";
+$data = mysql_query ($query)or die(mysql_error());
+if($data)
+{
+echo "YOUR REGISTRATION IS COMPLETED...";
+}
+else
+{
+echo "Unknown Error!"
+}
