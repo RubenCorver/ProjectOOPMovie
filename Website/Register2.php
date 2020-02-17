1880e84cb0607eb94ddf4c59995351f45a7023fa
Nino <nino@caiway.net>
2020-02-17 10:55:56 +0100

    movie script prototype

--- Website/Register2.php
+++ Website/Register2.php
@@ -2,6 +2,7 @@
 <html>
         <body>
 <?php
+            session_start();
             print $_POST["username"];
             print $_POST["password"];
             $iduser = NULL;
@@ -14,10 +15,17 @@
             
             if (mysqli_query($conn, $sql)) {
             echo "New record created successfully";
+                
                     } else {
                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
                     }
                     mysqli_close($conn);
+
+
+                if (!isset($_SESSION['loggedin'])) {
+	           header('Location: Wishlist.php');
+	           exit();
+                }
             ?>
     </body>
 </html>
