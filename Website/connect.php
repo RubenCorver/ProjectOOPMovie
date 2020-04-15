c3917b3a980ba4c654926c811d4026bbc55fac06
Nino <nino@caiway.net>
2020-04-15 15:55:18 +0200

    LocalConnect

--- Website/connect.php
+++ Website/connect.php
@@ -1,8 +1,8 @@
 <?php
-$dbServerName = "remotemysql.com:3306";
-$dbUsername = "aQ4VHAUFSM";
-$dbPassword = "L8fuvCAclt";
-$dbName = "aQ4VHAUFSM";
+$dbServerName = "localhost:3306";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "oopmovie";
 
 $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
 
